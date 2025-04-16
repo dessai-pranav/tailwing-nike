982091adf3407c602367b9d373f3980041c18e1a
pranav <kishrgdesai@gmail.com>
2025-04-16 23:29:12 +0530

    init

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const App = () => (
+
+       <main className= "realtive">
+           nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+            hero
+        </section>
+           <section className= " padding">
+               popular products
+           </section>
+           <section className= " padding">
+               Super Quality
+           </section>
+           <section className= " padding-x py-10">
+               service
+           </section>
+           <section className= " padding">
+               special offers
+           </section>
+           <section className= " bg-pale-blue padding">
+               customer reviews
+           </section>
+           <section className= " padding-x sm:py-32 py-16 w-full">
+               subscribe
+           </section>
+           <section className= " padding bg-black padding-x padding-t pb-8">
+              Footer
+           </section>
+       </main>
+)
+
+export default App
