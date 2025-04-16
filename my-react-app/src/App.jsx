6f9564f624ba3b08ebbb3492d0eeea003fb44736
pranav <kishrgdesai@gmail.com>
2025-04-17 00:12:28 +0530

    initialising all

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
+import Hero from "./section/Hero.jsx";
+import SpecialOffer from "./section/SpecialOffer.jsx";
+import Popular from "./section/Popular.jsx";
+import Footer from "./section/Footer.jsx";
+import CustomerReviews from "./section/CustomerReviews.jsx";
+import Services from "./section/Services.jsx";
+import Subscribe from "./section/Subscribe.jsx";
+import SuperQuality from "./section/SuperQuality.jsx";
+
+
 
 const App = () => (
 
        <main className= "realtive">
-           nav
+           { /* <Nav/> */}
         <section className="xl:padding-1 wide:padding-r padding-b">
-            hero
+            <Hero/>
         </section>
            <section className= " padding">
-               popular products
+               <Popular/>
            </section>
            <section className= " padding">
-               Super Quality
+               <SuperQuality/>
            </section>
            <section className= " padding-x py-10">
-               service
+               <Services/>
            </section>
            <section className= " padding">
-               special offers
+               <SpecialOffer/>
            </section>
            <section className= " bg-pale-blue padding">
                customer reviews
            </section>
            <section className= " padding-x sm:py-32 py-16 w-full">
-               subscribe
+               <Subscribe/>
            </section>
            <section className= " padding bg-black padding-x padding-t pb-8">
-              Footer
+              <Footer/>
            </section>
        </main>
 )
