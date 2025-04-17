08e02871eff975287ba2ee5cf53c70372b7ca3f1
pranav <kishrgdesai@gmail.com>
2025-04-17 22:59:34 +0530

    popular-product

--- my-react-app/src/components/PopularProductCard.jsx
+++ my-react-app/src/components/PopularProductCard.jsx
@@ -1,8 +1,19 @@
+import {star} from "../nike_landing_assets/assets/icons/index.js";
 
 
-const PopularProductsCard = () => {
+const PopularProductsCard =  ({imgURL,name ,price })=> {
     return (
-        <div>PopularProductCard</div>
+        <div className = " flex  flex-col w-full max-sm:w-full" >
+            <img
+                src = {imgURL} alt={name}
+                className = "w-[280px] h-[280px]"/>
+            <div className = "mt-8 flex justify-start gap-2.5">
+                <img src={star} alt = "rating" width ={24} height={24}/>
+                <p className= "font-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
+            </div>
+            <h3 className= " text-2xl leading-normal font-semibold font palanquin">{name}</h3>
+            <p className="mt-2  font-semibold font-montserrat leading-normal font-2xl ">{price}</p>
+        </div>
     )
 }
 export default PopularProductsCard
