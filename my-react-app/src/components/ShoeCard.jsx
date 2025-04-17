18fb1c031b7941fc2d63a56be82e8a09ff800a41
pranav <kishrgdesai@gmail.com>
2025-04-17 21:20:03 +0530

    url

--- my-react-app/src/components/ShoeCard.jsx
+++ my-react-app/src/components/ShoeCard.jsx
@@ -11,7 +11,7 @@ const ShoeCard = ({imgURL, changeBigShoeImage,bigShoeImage}) => {
         <div className={` border-2 rounded-x1
         ${bigShoeImage===imgURL ? 'border-coral-red ' : 'border-transparent'} cursor-pointer max-sm:flex-1` }
         onClick={handleClick}>
-            <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+            <div className='flex justify-center items-center bg-[url(/home/pranavgaunsdessai/Documents/tailwind/tailwing-nike/tailwing-nike/my-react-app/src/nike_landing_assets/assets/thumbnail-background.svg)] bg-center bg-cover bg-no-repeat sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
                 <img
                     src={imgURL.thumbnail}
                     alt='shoe colletion'
--- my-react-app/src/nike_landing_assets/assets/images/index.js
+++ my-react-app/src/nike_landing_assets/assets/images/index.js
@@ -16,7 +16,7 @@ import shoe6 from './shoe6.svg'
 import shoe7 from './shoe7.svg'
 import shoe8 from './shoe8.svg'
 
-import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailBackground from '../thumbnail-background.svg'
 import thumbnailShoe1 from './thumbnail-shoe1.svg'
 import thumbnailShoe2 from './thumbnail-shoe2.svg'
 import thumbnailShoe3 from './thumbnail-shoe3.svg'
