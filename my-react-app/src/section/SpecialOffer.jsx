a46236b382373762468d08731396c316012cd1f8
pranav <kishrgdesai@gmail.com>
2025-04-17 23:59:09 +0530

    services

--- my-react-app/src/section/SpecialOffer.jsx
+++ my-react-app/src/section/SpecialOffer.jsx
@@ -3,7 +3,11 @@ import {arrowRight} from "../nike_landing_assets/assets/icons/index.js";
 
 const SpecialOffer = () => {
     return (
-<div>hello </div>
+<section className = "flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+<div className=  " flex-1">
+
+</div>
+</section>
     )
 }
 export default SpecialOffer
