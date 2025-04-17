a48e00bfe37587ccf317df0a54b22306831cd68b
pranav <kishrgdesai@gmail.com>
2025-04-18 00:38:24 +0530

    SpecialOffer.jsx

--- my-react-app/src/components/button.jsx
+++ my-react-app/src/components/button.jsx
@@ -1,8 +1,9 @@
 import Nav from "./Nav.jsx";
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL, backgroundColor ,textColor , borderColor}) => {
     return (
-        <button className= "flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button className= {`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+         ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor} ` : "bg-coral-red rounded-full text-white border-coral-red"}`}>
             {label}
             {iconURL && <img
             src= {iconURL}
